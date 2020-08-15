55cd9607bda3b38ff34e06a0fc7e3ba8dcc8999c
Emmanuel Orubele <blontf@gmail.com>
2020-08-15 16:47:39 -0400

    fixed naming convention

--- src/main/java/com/github/perschola/WaitNotifyTest.java
+++ src/main/java/com/github/perschola/WaitNotifyTest.java
@@ -4,11 +4,11 @@ public class WaitNotifyTest {
     public static void main(String[] args) {
         Message msg = new Message("process it");
         Waiter waiter1 = new Waiter(msg);
-        Thread myThread = new Thread(waiter1);
-        myThread.start();
+        Thread threadOne = new Thread(waiter1, "waiter One");
+        threadOne.start();
         Waiter waiter2 = new Waiter(msg);
-        Thread myThreadTwo = new Thread(waiter2);
-        myThreadTwo.start();
+        Thread threadTwo = new Thread(waiter2, "waiter Two");
+        threadTwo.start();
         Thread notifier = new Thread(new Notifier(msg), "notifier");
         notifier.start();
         System.out.println("All the thread are started");
